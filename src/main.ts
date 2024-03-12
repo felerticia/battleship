c3aac4e261595f69400837894f81694f432a6419
Amir Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-03-12 20:15:19 +0100

    Grid

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 
+// Rotate options
 let isFlipped = false;
-
 const rotate = () => {
   const shipsHolder = document.getElementById("ships")!;
   const ships = [...shipsHolder.children] as HTMLElement[];
@@ -15,3 +15,18 @@ const rotate = () => {
 };
 
 document.getElementById("rotate")!.addEventListener("click", rotate);
+
+// Board
+const createGrid = (element: HTMLElement) => {
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = `cell-${i}`;
+    element.append(cell);
+  }
+};
+const playerBoard = document.getElementById("player")!;
+createGrid(playerBoard);
+
+const computerBoard = document.getElementById("computer")!;
+createGrid(computerBoard);
